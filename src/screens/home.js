a7d7c7cbaa9ce2ad4fe6f6f73383904e1a2a8241
sudheer <sudheergupta0508@gmail.com>
2021-09-26 23:32:02 +0530

    code cleanup & get done with labels

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,130 +1,147 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Modal, ScrollView, Text, TextInput, TouchableOpacity, FlatList,StatusBar} from 'react-native'
-import * as theme from '../constants/theme'
-import * as company from '../constants/jobs';
+import {
+  StyleSheet,
+  View,
+  Modal,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  StatusBar,
+} from 'react-native';
+import * as theme from '../constants/theme';
+import * as company from '../constants/patient';
 import Pateint from '../components/pateint';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import FilterModal from '../components/filterModal'
+import FilterModal from '../components/filterModal';
 
-export default function Home ({ navigation }) {
-    const [filterVisible, setFilterVisible] = useState(false)
+export default function Home({navigation}) {
+  const [filterVisible, setFilterVisible] = useState(false);
 
-    const ToggleFilterVisible = () => {
-        setFilterVisible(!filterVisible)
-    }
-    return(            
-        <>
-       
-       <View style={{flex: 1}}>
-           <Modal 
-                animationType="slide" 
-                visible={filterVisible}
-                onRequestClose={() => ToggleFilterVisible()}>
-                    <FilterModal closeModal={() => ToggleFilterVisible()} />
-            </Modal>
+  const ToggleFilterVisible = () => {
+    setFilterVisible(!filterVisible);
+  };
+  return (
+    <>
+      <View style={{flex: 1}}>
+        <Modal
+          animationType="slide"
+          visible={filterVisible}
+          onRequestClose={() => ToggleFilterVisible()}>
+          <FilterModal closeModal={() => ToggleFilterVisible()} />
+        </Modal>
 
-            <View>
+        <View>
+          {/* Safe area */}
+          <View style={styles.safeArea} />
 
-                {/* Safe area */}
-                <View  style={styles.safeArea}/>
-                 
-                {/* Body */}
-                  {/* Search patient */}
-                  <View>
-                        <Text style={styles.h4}>Search</Text>
-                    </View>
+          {/* Body */}
+          {/* Search patient */}
+          <View>
+            <Text style={styles.h4}>Search</Text>
+          </View>
 
-                    {/* Search */}
-                    <View style={styles.searchContainer}>
-                        <View style={styles.searchInputContainer}>
-                            <Icon name="search" size={30} color={theme.colors.silver} />
-                            <TextInput 
-                                placeholder='Enter patient name' />
-                        </View>
-                        <TouchableOpacity style={styles.searchIconContainer} onPress={() => ToggleFilterVisible()}>
-                            <Icon name="filter-list" size={30} color={theme.colors.white} />
-                        </TouchableOpacity>
-                    </View>
-
-                <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
-
-                  
-                    {/* Recent Patients */}
-                    <View style={[styles.popularContainer, {marginRight: 20, marginLeft: 20, marginBottom: 70}]}>
-                        <Text style={styles.popularText}>Recent Patients</Text>
-                        <FlatList 
-                            data={company.companies}
-                            keyExtractor={item => item.id}
-                            renderItem={({ item }) => {
-                                return (
-                                    <Pateint pateint={item} />
-                                )
-                            }} />
-                    </View>
-                </ScrollView>
+          {/* Search */}
+          <View style={styles.searchContainer}>
+            <View style={styles.searchInputContainer}>
+              <Icon name="search" size={30} color={theme.colors.silver} />
+              <TextInput placeholder="Enter patient name" />
             </View>
-       </View>
-   </>
-    )
+            <TouchableOpacity
+              style={styles.searchIconContainer}
+              onPress={() => ToggleFilterVisible()}>
+              <Icon name="filter-list" size={30} color={theme.colors.white} />
+            </TouchableOpacity>
+          </View>
+          <View
+            style={[
+              styles.popularContainer,
+              {marginRight: 20, marginLeft: 20},
+            ]}>
+            <Text style={styles.popularText}>Recent Patients</Text>
+          </View>
+          <ScrollView
+            style={styles.container}
+            showsVerticalScrollIndicator={false}>
+            {/* Recent Patients */}
+            <View
+              style={[
+                styles.popularContainer,
+                {marginRight: 20, marginLeft: 20, marginBottom: 70},
+              ]}>
+              <FlatList
+                data={company.companies}
+                keyExtractor={(item) => item.id}
+                renderItem={({item}) => {
+                  return <Pateint pateint={item} />;
+                }}
+              />
+            </View>
+          </ScrollView>
+        </View>
+      </View>
+    </>
+  );
 }
 
-const styles =StyleSheet.create({
-    safeArea: {
-        height:15,
-        backgroundColor: theme.colors.lightWhite,
-    },
-    header: {
-        padding: 20,
-        flexDirection: 'row',
-        backgroundColor: theme.colors.lightWhite,
-        justifyContent: 'space-between',
-        backgroundColor: 'red'
-    },
-    container: {
-        backgroundColor: theme.colors.lightWhite
-    },
-    h4: {
-        marginLeft: 20,
-        fontWeight: 'bold',
-        fontSize: theme.sizes.h4,
-        color: theme.colors.black
-    },
-    title: {
-        marginLeft: 20,
-        fontWeight: 'bold',
-        fontSize: theme.sizes.h6,
-        color: theme.colors.black
-    },
-    searchContainer: {
-        marginTop: 15,
-        marginLeft: 20,
-        paddingRight: 20,
-        flexDirection: 'row',
-        justifyContent: 'flex-start'
-    },
-    searchInputContainer: {
-        flex: 1,
-        padding: 5,
-        borderRadius: 5,
-        alignItems: 'center',
-        flexDirection: 'row',
-        backgroundColor: theme.colors.white
-    },
-    searchIconContainer: {
-        padding: 12,
-        marginLeft: 10,
-        borderRadius: 5,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: theme.colors.black
-    },
-    popularContainer: {
-        paddingTop: 20,
-    },
-    popularText: {
-        marginBottom: 15,
-        fontWeight: 'bold',
-        fontSize: theme.sizes.h3,
-        color: theme.colors.black
-    }
-})
+const styles = StyleSheet.create({
+  safeArea: {
+    height: 15,
+    backgroundColor: theme.colors.lightWhite,
+  },
+  header: {
+    padding: 20,
+    flexDirection: 'row',
+    backgroundColor: theme.colors.lightWhite,
+    justifyContent: 'space-between',
+    backgroundColor: 'red',
+  },
+  container: {
+    backgroundColor: theme.colors.lightWhite,
+  },
+  h4: {
+    marginLeft: 20,
+    fontWeight: 'bold',
+    fontSize: theme.sizes.h4,
+    color: theme.colors.black,
+  },
+  title: {
+    marginLeft: 20,
+    fontWeight: 'bold',
+    fontSize: theme.sizes.h6,
+    color: theme.colors.black,
+  },
+  searchContainer: {
+    marginTop: 15,
+    marginLeft: 20,
+    paddingRight: 20,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  searchInputContainer: {
+    flex: 1,
+    padding: 5,
+    borderRadius: 5,
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: theme.colors.white,
+  },
+  searchIconContainer: {
+    padding: 12,
+    marginLeft: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: theme.colors.black,
+  },
+  popularContainer: {
+    paddingTop: 20,
+  },
+  popularText: {
+    marginBottom: 15,
+    fontWeight: 'bold',
+    fontSize: theme.sizes.h3,
+    color: theme.colors.black,
+  },
+});
